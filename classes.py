54e317cf69310483f6db0949470684b414fa3721
Oscar Martinez <145166121+Oscaretz@users.noreply.github.com>
2024-01-29 21:27:12 -0600

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,4 +1,10 @@
-
+import pandas as pd
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import statsmodels.api as sm
+import csv
 
 '''
 Cleaner, clase para recibir un archivo CSV y limpiarlo.
